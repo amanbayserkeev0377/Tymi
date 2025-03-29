afe9db49519be19d8a676a157cce8965bbd5dc73
Aman Bayserkeev <68231005+amanbayserkeev0377@users.noreply.github.com>
2025-03-29 21:44:58 +0600

    init commit

---
+++ Tymi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tymi
+//
+//  Created by Aman on 29/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tymi/TymiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TymiApp.swift
+//  Tymi
+//
+//  Created by Aman on 29/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TymiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
