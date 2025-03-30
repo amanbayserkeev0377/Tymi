13e488036152c04edc04c7a5976525dbc2d881aa
Aman Bayserkeev <68231005+amanbayserkeev0377@users.noreply.github.com>
2025-03-30 09:34:27 +0600

    Add HabitType Model

---
+++ Tymi/Models/HabitType.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+enum HabitType: String, Codable {
+    case count
+    case time
+}
+
+enum Weekday: Int, CaseIterable, Codable {
+    case monday = 1
+    case tuesday = 2
+    case wednesday = 3
+    case thursday = 4
+    case friday = 5
+    case saturday = 6
+    case sunday = 7
+    
+    var shortName: String {
+        switch self {
+        case .monday: return "Mon"
+        case .tuesday: return "Tue"
+        case .wednesday: return "Wed"
+        case .thursday: return "Thu"
+        case .friday: return "Fri"
+        case .saturday: return "Sat"
+        case .sunday: return "Sun"
+        }
+    }
+} 
